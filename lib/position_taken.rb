72427596c87036375205ce335c9a0388e6bdc27a
yurina shin <yurinas@gmail.com>
2017-11-14 23:41:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 
 board = ["  ","  ","  ","  ","  ","  ","  ","  ","  "]
 def position_taken?(board,index)
-  if board[index] != "  "
-    false
   if board[index] != " "
+    false
+  if board[index] != ""
 false
 if board[index] != nil
   false
